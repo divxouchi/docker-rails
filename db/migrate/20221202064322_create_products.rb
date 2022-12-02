997e949e87372cd5994f12b205d3e6587a197027
divxouchi <manabu.ouchi@divx.co.jp>
2022-12-02 15:59:17 +0900

    products テーブル作成

--- db/migrate/20221202064322_create_products.rb
+++ db/migrate/20221202064322_create_products.rb
@@ -1,7 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-
+      t.string :title
+      t.integer :money
+      t.string :content
       t.timestamps
     end
   end
